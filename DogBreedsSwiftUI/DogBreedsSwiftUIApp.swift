bb4230441417e842bdae199d701ad724a0a2fcbb
Parthiv Ganguly <parthiv2048@gmail.com>
2026-02-23 10:30:51 -0500

    Initial Commit

---
+++ DogBreedsSwiftUI/DogBreedsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedsSwiftUIApp.swift
+//  DogBreedsSwiftUI
+//
+//  Created by Parthiv Ganguly on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct DogBreedsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
